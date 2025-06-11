baf7891960de2bf6a016e466050838fa8adb16db
waldembergdev <waldemberg.pereira@outlook.com>
2025-06-11 04:57:22 -0300

    feat: alterado a função de enviar e-mail, criado o base_email.html e inscriacao.html

--- utils/enviar_email.py
+++ utils/enviar_email.py
@@ -1,12 +1,48 @@
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 from django.conf import settings
+from enum import Enum
 
-def enviar_email(assunto, mensagem, destinatario):
-    send_mail(
-        assunto,
-        mensagem,
-        settings.DEFAULT_FROM_EMAIL,
-        [destinatario],
-        fail_silently=False  #  True para ignorar erros, False para levantar exceção
-    )
-    return True  # ou False para erros
+# definindo os locais do html
+class TipoEmail(Enum):
+    INSCRICAO = ('emails/inscricao.html', 'Confirmação de inscriação!')
+
+    @property
+    def template_path(self):
+        """Retorna o caminho do template HTML associado ao tipo de e-mail."""
+        return self.value[0]
+
+    @property
+    def assunto(self):
+        """Retorna o assunto padrão associado ao tipo de e-mail."""
+        return self.value[1]
+
+
+
+def enviar_email(tipo_email: TipoEmail, email_destinatario: str, nome_destinatario: str):
+    # defindo as variaveis principais
+    assunto = tipo_email.assunto
+    remetente = settings.DEFAULT_FROM_EMAIL # Use um e-mail de remetente válido
+
+    # local do html
+    template_path =  tipo_email.value
+
+    # 2. Variáveis do template
+    context = {
+        'nome_usuario': nome_destinatario,
+    }
+
+    html_content = render_to_string(template_path, context)
+
+    # Versão em texto puro 
+    text_content = f"Olá {context['nome_usuario']}"
+
+    
+    message = EmailMultiAlternatives(assunto, text_content, remetente, [email_destinatario])
+    message.attach_alternative(html_content, "text/html")
+
+    try:
+        message.send()
+        print("E-mail enviado com sucesso!")
+    except Exception as e:
+        print(f"Erro ao enviar e-mail: {e}")
