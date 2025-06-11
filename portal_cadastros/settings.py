ddd578eb8eac5234e1ee118e47a9a040e1b59e2c
waldembergdev <waldemberg.pereira@outlook.com>
2025-06-10 21:25:38 -0300

    chore: adicionado o arquivo .env

--- portal_cadastros/settings.py
+++ portal_cadastros/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from django.contrib.messages import constants
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,11 +145,11 @@ AUTH_USER_MODEL = 'usuarios.Usuario'
 # configurações de envio de e-mail
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.zoho.com' 
-EMAIL_PORT = 587  
-EMAIL_HOST_USER = 'waldemberg@grupoqualityambiental.com.br'
-EMAIL_HOST_PASSWORD = 'wyta7NPAVFzB'
+EMAIL_HOST = os.getenv('EMAIL_HOST') 
+EMAIL_PORT = os.getenv('EMAIL_PORT')  
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 EMAIL_USE_TLS = True 
 EMAIL_USE_SSL = False
-DEFAULT_FROM_EMAIL = 'waldemberg@grupoqualityambiental.com.br'
 
